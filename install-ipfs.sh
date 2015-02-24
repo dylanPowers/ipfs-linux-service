86b34a314988a53e2deb0d0b11e2eddd4ca93bf0
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-02-23 21:29:38 -0800

    install: init daemon now configured to run at boot
    Also updated some of the messages given to the user

--- install-ipfs.sh
+++ install-ipfs.sh
@@ -16,6 +16,8 @@ IPFS_USER=ipfs-daemon
 ## Note: Copying rather than linking avoids permissions problems
 cp $GOPATH/bin/ipfs /usr/local/bin/ipfs
 if [ ! -d $IPFS_PATH ]; then mkdir -p $IPFS_PATH; fi
+
+echo "Initializing ipfs... This can take some time to generate the keys"
 ipfs init >/dev/null
   
 ## Check if the daemon user exists
@@ -27,16 +29,17 @@ chown -R $IPFS_USER:$IPFS_USER $IPFS_PATH
 
 cp init.d/ipfs /etc/init.d/ipfs
 chmod +x /etc/init.d/ipfs
+update-rc.d ipfs defaults >/dev/null
 
 msg="
-*********************************  Success!  ***********************************
+****************************  Daemon Installed!  *******************************
 The ipfs daemon has now been installed. You may now start it as a system
 service. With most distros you can run the following to start the service:
                         \`sudo service ipfs start\`
 
 The ipfs configuration can be found at $IPFS_PATH/
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  One last step  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+However, one last step!!!
 The IPFS_PATH environment variable with the location of the ipfs 
 configuration must be loaded into your shell when running ipfs commands. 
 So add into your .bashrc:
