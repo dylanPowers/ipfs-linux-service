b76ed5c758edc928c8d4379a27cb36a52aba0287
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-04-05 19:02:16 -0700

    fix: binary not properly being copied over to /usr/local/bin

--- update-ipfs.sh
+++ update-ipfs.sh
@@ -12,6 +12,6 @@ if [ $1 != "--no-download" ]; then
 fi
 
 echo "Stopping the service and copying the binary over. This will require root."
-sudo service ipfs stop && \
-  sudo cp $GOPATH/bin/ipfs /usr/local/bin/ipfs
+sudo service ipfs stop 
+sudo cp $GOPATH/bin/ipfs /usr/local/bin/ipfs
 sudo service ipfs start
